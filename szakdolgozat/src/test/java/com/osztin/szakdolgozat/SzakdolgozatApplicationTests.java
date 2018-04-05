f74e0ed429eb7210a65b26406bedaea708291e4d
osztin <suru.gyula@gmail.com>
2018-04-05 14:06:11 +0200

    first commit

---
+++ szakdolgozat/src/test/java/com/osztin/szakdolgozat/SzakdolgozatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.osztin.szakdolgozat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SzakdolgozatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
